e9b894b3a45d7488f744fa715cc1762d7865e4eb
Kerry Schaffer <kscha@ITs-MacBook-Pro.local>
2013-11-12 09:42:42 -0500

    update for test

---
+++ vendor/bundle/gems/json-1.8.1/ext/json/ext/generator/siteconf20131107-51892-yk3vzl.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131107-51892-2ha4t6"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
